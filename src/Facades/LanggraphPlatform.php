6808e4e6696118bc680a2dc569b4f056c9c4b261
Jason Tame <jasonmiketame2@gmail.com>
2025-07-19 13:02:53 +0200

    Add facade docblock

--- src/Facades/LanggraphPlatform.php
+++ src/Facades/LanggraphPlatform.php
@@ -1,10 +1,24 @@
 <?php
 
+declare(strict_types=1);
+
 namespace LangGraphPlatform\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * Facade for LangGraph Platform SDK.
+ *
+ * @method static \LangGraphPlatform\Resources\AssistantsClient assistants()
+ * @method static \LangGraphPlatform\Resources\ThreadsClient threads()
+ * @method static \LangGraphPlatform\Resources\RunsClient runs()
+ * @method static \LangGraphPlatform\Resources\CronsClient crons()
+ * @method static \LangGraphPlatform\Resources\StoreClient store()
+ * @method static \LangGraphPlatform\Http\Client getHttpClient()
+ * @method static \LangGraphPlatform\LangGraphPlatform configure(array $config)
+ * @method static \LangGraphPlatform\LangGraphPlatform create(array $config = [])
+ * @method static \LangGraphPlatform\LangGraphPlatform fromEnvironment()
+ *
  * @see \LangGraphPlatform\LangGraphPlatform
  */
 class LangGraphPlatform extends Facade
