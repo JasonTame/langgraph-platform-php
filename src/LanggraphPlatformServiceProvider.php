38148de6d7ec630958d8546f9cad5008620670bb
Jason Tame <jasonmiketame2@gmail.com>
2025-07-19 13:03:10 +0200

    Register langgraph platform as a singleton

--- src/LanggraphPlatformServiceProvider.php
+++ src/LanggraphPlatformServiceProvider.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace LangGraphPlatform;
 
 use LangGraphPlatform\Commands\LangGraphPlatformCommand;
@@ -21,5 +23,23 @@ class LangGraphPlatformServiceProvider extends PackageServiceProvider
             ->hasViews()
             ->hasMigration('create_langgraph_platform_php_table')
             ->hasCommand(LangGraphPlatformCommand::class);
+    }
+
+    public function registeringPackage(): void
+    {
+        // Register the main LangGraph Platform client as a singleton
+        $this->app->singleton(LangGraphPlatform::class, function ($app) {
+            return new LangGraphPlatform();
+        });
+
+        // Bind the client to the container using the interface if needed
+        $this->app->bind('langgraph-platform', function ($app) {
+            return $app->make(LangGraphPlatform::class);
+        });
+    }
+
+    public function packageBooted(): void
+    {
+        // Additional setup can be done here if needed
     }
 }
